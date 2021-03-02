6ebee0a8c5b04bddb1da30cab8a23c1a31055f56
raval-64 <nabhani064@gmail.com>
2021-03-02 21:31:58 +0530

    virtual env remove.

--- movie_api/movie/views.py
+++ movie_api/movie/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.shortcuts import render, get_object_or_404, redirect,HttpResponse
 import requests
+
 # Create your views here.
 def index(request):
     sender = {'index':True}
